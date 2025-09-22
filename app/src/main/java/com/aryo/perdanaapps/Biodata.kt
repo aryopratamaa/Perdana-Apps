0d49aff83edd0b7c43f5182e26201115b10660a2
ARYO PRATAMA <mr.aryopratama@gmail.com>
2025-09-22 15:57:34 +0700

    7

---
+++ app/src/main/java/com/aryo/perdanaapps/Biodata.kt
@@ -0,0 +1,16 @@
+package com.aryo.perdanaapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Biodata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.biodata)
+    }
+
+
+
+}
--- app/src/main/java/com/aryo/perdanaapps/MainActivity.kt
+++ app/src/main/java/com/aryo/perdanaapps/MainActivity.kt
@@ -3,18 +3,37 @@ package com.aryo.perdanaapps
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.content.Intent
+import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnHome = findViewById<Button>(R.id.btnHome)
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+        val btnKeluar = findViewById<Button>(R.id.btnKeluar)
+        btnHome.setOnClickListener {
+            Toast.makeText(this, "Home ditekan", Toast.LENGTH_SHORT).show()
         }
+        btnSettings.setOnClickListener {
+            Toast.makeText(this, "Settings ditekan", Toast.LENGTH_SHORT).show()
+        }
+        btnKeluar.setOnClickListener {
+            finishAffinity()
+        }
+
+        val button = findViewById<Button>(R.id.buttonGoToBiodata)
+        button.setOnClickListener {
+            val intent = Intent(this, Biodata::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
